52e6628d92a833875e6ec8097bf82483acfa1514
Sheila Kahwai <114513578+Sheila-nk@users.noreply.github.com>
2023-03-19 21:13:27 +0300

    set up app onfiguration

--- user_authenticator/__init__.py
+++ user_authenticator/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from user_authenticator.config import ApplicationConfig
 
 app = Flask(__name__)
+app.config.from_object(ApplicationConfig)
+db = SQLAlchemy(app)
 
 from user_authenticator import views
