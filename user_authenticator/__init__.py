88184db774543076560f496dab23c03c4edf5658
Sheila Kahwai <114513578+Sheila-nk@users.noreply.github.com>
2023-03-21 20:58:20 +0300

    setup migrations

--- user_authenticator/__init__.py
+++ user_authenticator/__init__.py
@@ -1,17 +1,25 @@
+from sqlalchemy import MetaData
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
 from user_authenticator.config import ApplicationConfig
 
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
-db = SQLAlchemy(app)
+db = SQLAlchemy(app, metadata=metadata)
 bcrypt = Bcrypt(app)
-
-from user_authenticator.models import User
-
-with app.app_context():
-    db.create_all()
+migrate = Migrate(app, db, render_as_batch=True)
 
 from user_authenticator import views
